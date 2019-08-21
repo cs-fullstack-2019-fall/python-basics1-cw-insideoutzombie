e50b5e1c60538177ea1a0602b2696ec73ee0763f
insideoutzombie <david@web3devs.com>
2019-08-21 11:20:15 -0500

    initial commit

---
+++ index.py
@@ -0,0 +1,15 @@
+### Problem 1:
+# Create two variables. One should equal “My name is: “ and the other should equal your actual name.
+# Print the two variables in one print message.
+var1 = "My name is: "
+var2 = input("Enter your name ")
+print(var1, var2)
+
+### Problem 2:
+#Ask the user to enter the extra credit they earned. If they entered less than 5 print “That’s not enough extra credit.” If they entered more than 20 print “That’s too much extra credit”.
+
+### Problem 3:
+#Ask a user to enter a password. Enter a password. Ask user to reenter password. Check to see if they are correct.
+
+### Problem 4:
+#Ask for two card numbers. If it equals 21 print BLACKJACK!, if it’s greater than 21 print BUST!, if it’s less than 21 print “The total is [THE TOTAL]”
