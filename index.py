f20fb757cf16426ee0960584b42e405db9b4c0a7
autumn-ragland <raglanda998@gmail.com>
2019-08-21 14:12:41 -0500

    Graded 08-21-2019 14:11:46

--- index.py
+++ index.py
@@ -1,33 +1,33 @@
 ### Problem 1:
 # Create two variables. One should equal “My name is: “ and the other should equal your actual name.
 # Print the two variables in one print message.
-# var1 = "My name is: "
-# var2 = input("Enter your name ")
-# print(var1, var2)
+var1 = "My name is: "
+var2 = input("Enter your name ")
+print(var1, var2)
 
 ### Problem 2:
 #Ask the user to enter the extra credit they earned.
 # If they entered less than 5 print “That’s not enough extra credit.”
 # If they entered more than 20 print “That’s too much extra credit”.
-# userInput = int(input("enter the extra credit you earned "))
-# if(userInput<5):
-#     print("That’s not enough extra credit")
-# elif(userInput>20):
-#     print("That’s too much extra credit")
-# else:
-#     print(userInput)
+userInput = int(input("enter the extra credit you earned "))
+if(userInput<5):
+    print("That’s not enough extra credit")
+elif(userInput>20):
+    print("That’s too much extra credit")
+else:
+    print(userInput)
 
 
 
 ### Problem 3:
 #Ask a user to enter a password. Enter a password. Ask user to reenter password. Check to see if they are correct.
 
-# userPassword = input("Enter a password ")
-# userPassword2 = input("Reenter the passwrod")
-# if(userPassword==userPassword2):
-#     print("They match")
-# else:
-#     print("Incorrect")
+userPassword = input("Enter a password ")
+userPassword2 = input("Reenter the passwrod")
+if(userPassword==userPassword2):
+    print("They match")
+else:
+    print("Incorrect")
 
 ### Problem 4:
 # Ask for two card numbers.
